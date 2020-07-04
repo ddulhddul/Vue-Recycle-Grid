c28e7bc5328b85b7476518af5bcc235f4a62cb12
ddulh <rjw88@naver.com>
2020-07-04 13:53:48 +0900

    'init'

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>asfasf</h1>  
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        exclude: /node_modules/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
+};
